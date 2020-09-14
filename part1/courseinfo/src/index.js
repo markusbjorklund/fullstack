5e63443abfce7e64775aaa4db4b2908ecafc3d85
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-09-14 12:42:33 +0300

    1.1 done

--- part1/courseinfo/src/index.js
+++ part1/courseinfo/src/index.js
@@ -19,7 +19,6 @@ const Total = (props) => {
   )
 }
 
-
 const App = () => {
   const course = 'Half Stack application development'
   const part1 = 'Fundamentals of React'
@@ -30,13 +29,13 @@ const App = () => {
   const exercises3 = 14
 
   return (
-    <div>
+    <>
       <Header course={course} />
       <Content part={part1} exercises={exercises1} />
       <Content part={part2} exercises={exercises2} />
       <Content part={part3} exercises={exercises3} />
       <Total exercises={exercises1 + exercises2 + exercises3} />
-    </div>
+    </>
   )
 }
 
