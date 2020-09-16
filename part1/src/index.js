7a6925335baecad363acb4e110b6d0eefe2b90e7
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-09-16 12:40:24 +0300

    fix gitignore

---
+++ part1/src/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Hello = (props) => {
+  return (
+    <>
+      <p>Hello world {props.name}, you are {props.age} years old</p>
+    </>
+  )
+}
+
+const App = () => {
+  const name = 'Peter'
+  const age = 10
+
+  return (
+    <>
+      <h1>Greetings</h1>
+      <Hello name="Maya" age={26 + 10} />
+      <Hello name={name} age={age} />
+      <Hello />
+    </>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
