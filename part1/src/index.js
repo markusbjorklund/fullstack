f74e537eafadb380841e9c5e4c79f731e409e3fe
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-09-16 14:24:30 +0300

    testing

--- part1/src/index.js
+++ part1/src/index.js
@@ -1,11 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-const Hello = (props) => {
+const Hello = ({name, age}) => {
+    const bornYear = () => new Date().getFullYear() - age
+
   return (
-    <>
-      <p>Hello world {props.name}, you are {props.age} years old</p>
-    </>
+    <div>
+      <p>
+        Hello {name}, you are {age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </div>
   )
 }
 
@@ -14,12 +19,11 @@ const App = () => {
   const age = 10
 
   return (
-    <>
+    <div>
       <h1>Greetings</h1>
       <Hello name="Maya" age={26 + 10} />
       <Hello name={name} age={age} />
-      <Hello />
-    </>
+    </div>
   )
 }
 
