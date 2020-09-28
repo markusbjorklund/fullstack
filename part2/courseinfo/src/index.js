fa61b119653df4c702ac746bb9ab9aefd6afeafb
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-09-28 16:54:39 +0300

    2.1 done

--- part2/courseinfo/src/index.js
+++ part2/courseinfo/src/index.js
@@ -8,12 +8,10 @@ const Header = ({ course }) => {
 }
 
 const Total = ({ course }) => {
-  const total = course.parts.reduce((sum, currentValue) =>
-    sum + currentValue.exercises, 0)
-    
-  return (
-    <p><b>total of {total} exercises</b></p>
-  )
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return(
+    <p>Number of exercises {sum}</p>
+  ) 
 }
 
 const Part = (props) => {
@@ -46,36 +44,28 @@ const Course = ({ course }) => {
 
 const App = () => {
   const course = {
+    id: 1,
     name: 'Half Stack application development',
     parts: [
       {
         name: 'Fundamentals of React',
-        exercises: 10
+        exercises: 10,
+        id: 1
       },
       {
         name: 'Using props to pass data',
-        exercises: 7
+        exercises: 7,
+        id: 2
       },
       {
         name: 'State of a component',
-        exercises: 14
-      },
-      {
-        name: 'Some new',
-        exercises: 10
-      },
-      {
-        name: 'More new',
-        exercises: 15
+        exercises: 14,
+        id: 3
       }
     ]
   }
 
-  return (
-    <div>
-      <Course course={course} />
-    </div>
-  )
+  return <Course course={course} />
 }
 
 ReactDOM.render(<App />, document.getElementById('root'))
