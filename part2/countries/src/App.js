63f0df0aa739eed9346d77f03a4387d0a0cc3069
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-09-30 21:36:56 +0300

    2.12 done

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,6 +1,68 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 
+const Filter = (props) => {
+  return (
+    <>
+      <div>
+        find countries:
+      <input
+          value={props.value}
+          onChange={props.onChange}
+        />
+      </div>
+    </>
+  )
+}
+
+const ShowCountries = ({ filteredCountries, countries }) => {
+  if (filteredCountries.length === Math.max(countries.length)) {
+    return (
+      <>
+      </>
+    )
+  }
+
+  if (filteredCountries.length === 1) {
+    return (
+      <>
+        {filteredCountries.map(country =>
+          <div key={country.name}>
+            <h2>{country.name}</h2>
+            <p>capital {country.capital}</p>
+            <p>population {country.population}</p>
+            <h3>languages</h3>
+            <ul>
+              {country.languages.map(lang =>
+                <li key={lang.iso639_1}>{lang.name}</li>
+              )}
+            </ul>
+            <img src={country.flag} alt="country flag" width="150" />
+          </div>
+        )}
+      </>
+    )
+  }
+
+  if (filteredCountries.length < 10) {
+    return (
+      <>
+        {filteredCountries.map(country => (
+          <p key={country.name}> {country.name}</p>
+        ))}
+      </>
+    )
+  }
+
+  else if (filteredCountries.length > 10 || filteredCountries.length - 1) {
+    return (
+      <>
+        <p>Too many matches, specify another filter</p>
+      </>
+    )
+  }
+}
+
 const App = () => {
   const [countries, setCountries] = useState([])
   const [countriesFilter, setCountriesFilter] = useState('')
@@ -13,19 +75,16 @@ const App = () => {
       })
   }, [])
 
-  console.log('countries', countries)
-  console.log('countries length', countries)
+  const filteredCountries = countries.filter(country => country.name.toLowerCase().includes(countriesFilter.toLowerCase()))
+
+  const handleFilterChange = (event) => {
+    setCountriesFilter(event.target.value)
+  }
+
   return (
     <>
- 
-    {countries.map(country => {
-      return <div key={country.name}>
-              <h2>{country.name}</h2>
-              <p>capital {country.capital}</p>
-              <p>population {country.population}</p>
-              <img src={country.flag} width="100" alt={country.name} />
-          </div>
-    })}
+      <Filter value={countriesFilter} onChange={handleFilterChange} />
+      <ShowCountries filteredCountries={filteredCountries} countries={countries} />
     </>
   );
 }
