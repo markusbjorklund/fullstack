55df46e39d904412e71dcfd6270938f0d4a8ae82
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-10-27 15:08:23 +0200

    4.4 done

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -2,4 +2,11 @@ const dummy = (blogs) => {
   return 1
 }
 
-module.exports = { dummy }
+const totalLikes = (blogs) => {
+  return (
+    blogs.map(blog => blog.likes)
+      .reduce((total, likes) => total + likes, 0)
+  )
+}
+
+module.exports = { dummy, totalLikes }
