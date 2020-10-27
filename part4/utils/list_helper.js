52d423588beec8d70e002b51b1de84d912a5e7ef
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-10-27 17:44:22 +0200

    4.5 in progress

--- part4/utils/list_helper.js
+++ part4/utils/list_helper.js
@@ -9,4 +9,24 @@ const totalLikes = (blogs) => {
   )
 }
 
-module.exports = { dummy, totalLikes }
+const favoriteBlog = (blogs) => {
+  const likes = blogs.map(blog => blog.likes)
+
+  console.log(likes)
+
+  const object = blogs.find(blog => blog.likes === Math.max(...likes))
+
+  if (typeof object === 'undefined') return 0
+
+  console.log(object)
+
+  delete object._id
+  delete object.__v
+  delete object.url
+
+  console.log(object)
+  
+  return object
+}
+
+module.exports = { dummy, totalLikes, favoriteBlog }
