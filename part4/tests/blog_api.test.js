3c4e634cea0705ae89b6c58e076173b2f1f80965
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-10-30 12:38:34 +0200

    4.8 done

---
+++ part4/tests/blog_api.test.js
@@ -0,0 +1,41 @@
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+
+const Blog = require('../models/blog')
+const initialBlogs = [
+  {
+    title: 'Testblog 1',
+    author: 'Tester 1',
+    url: 'cool url 1',
+    likes: 2
+  },
+  {
+    title: 'Testblog 2',
+    author: 'Tester 2',
+    url: 'cool url 2',
+    likes: 4
+  },
+]
+beforeEach(async () => {
+  await Blog.deleteMany({})
+  await new Blog(initialBlogs[0]).save()
+  await new Blog(initialBlogs[1]).save()
+})
+
+test('blogs are returned as json', async () => {
+  await api
+    .get('/api/blogs')
+    .expect(200)
+    .expect('Content-Type', /application\/json/)
+})
+
+test('amount of blogs', async () => {
+  const blogs = await api.get('/api/blogs')
+  expect(blogs.body.length).toBe(2)
+})
+
+afterAll(() => {
+  mongoose.connection.close()
+})
