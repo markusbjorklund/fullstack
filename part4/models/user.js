93354ed3f1373374b1e27d151ad46a06a0d0b03e
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-10-31 11:13:47 +0200

    4.15 done

---
+++ part4/models/user.js
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose')
+const uniqueValidator = require('mongoose-unique-validator')
+
+const userSchema = mongoose.Schema({
+  username: {
+    type: String,
+    unique: true
+  },
+  name: String,
+  passWordHash: String
+})
+
+userSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+    delete returnedObject.passWordHash
+  }
+})
+
+userSchema.plugin(uniqueValidator)
+
+const User = mongoose.model('User', userSchema)
+
+module.exports = User
