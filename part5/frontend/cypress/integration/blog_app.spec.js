418f2d15d4686c88aebbe441944150138827fbf2
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-11-14 11:02:45 +0200

    5.17 done

---
+++ part5/frontend/cypress/integration/blog_app.spec.js
@@ -0,0 +1,16 @@
+describe('Blog app', function () {
+  beforeEach(function () {
+    cy.request('POST', 'http://localhost:3001/api/testing/reset')
+    const user = {
+      name: 'Snake Plissken',
+      username: 'snake',
+      password: 'salasana'
+    }
+    cy.request('POST', 'http://localhost:3001/api/users/', user)
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function () {
+    cy.contains('log in to application')
+  })
+})
