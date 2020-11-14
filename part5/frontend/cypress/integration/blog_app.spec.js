7706c6732590d489edfbecd458b92c14cac5aed0
Markus Bjorklund <markus.bjorklund@gmail.com>
2020-11-14 11:44:42 +0200

    5.19 done

--- part5/frontend/cypress/integration/blog_app.spec.js
+++ part5/frontend/cypress/integration/blog_app.spec.js
@@ -14,8 +14,8 @@ describe('Blog app', function () {
     cy.contains('log in to application')
   })
 
-  describe('Login', function() {
-    it('succeeds with correct credentials', function() {
+  describe('Login', function () {
+    it('succeeds with correct credentials', function () {
       cy.contains('login').click()
       cy.get('#username').type('snake')
       cy.get('#password').type('salasana')
@@ -23,7 +23,7 @@ describe('Blog app', function () {
       cy.contains('Snake Plissken logged in')
     })
 
-    it('fails with wrong credentials', function() {
+    it('fails with wrong credentials', function () {
       cy.contains('login').click()
       cy.get('#username').type('snake')
       cy.get('#password').type('wrong')
@@ -32,7 +32,26 @@ describe('Blog app', function () {
       cy.get('html').should('not.contain', 'Snake Plissken logged in')
       cy.get('.error').should('have.css', 'color', 'rgb(255, 0, 0)')
     })
+
+    describe.only('When logged in', function () {
+      beforeEach(function () {
+        cy.get('#username').type('snake')
+        cy.get('#password').type('salasana')
+        cy.get('#login-button').click()
+      })
+
+      it('A blog can be created', function () {
+        cy.contains('new blogpost').click()
+        cy.get('html').should('contain', 'create new')
+        cy.get('#title').type('A dark night in NY')
+        cy.get('#author').type('Snakeman')
+        cy.get('#url').type('https://en.wikipedia.org/wiki/Snake_Plissken')
+        cy.get('#create-button').click()
+
+        cy.contains('A dark night in NY by Snakeman')
+        cy.get('.success').should('contain', 'A dark night in NY by Snakeman added')
+
+      })
+    })
   })
-
-
 })
